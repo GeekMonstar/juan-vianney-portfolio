0289c5707c516c3d0e533e04c504a28b112f8d55
GeekMonstar <94204578+GeekMonstar@users.noreply.github.com>
2022-09-22 20:05:11 +0200

    Update appliMeteo.js

--- Projects/AppliMeteo/appliMeteo.js
+++ Projects/AppliMeteo/appliMeteo.js
@@ -1,33 +1,93 @@
-let ville = 'sidney';
+let villeRequest = 'Nantes';
+const form = document.querySelector('form');
+const ville = document.querySelector('#ville');
+const pays = document.querySelector('#pays');
+const temp = document.querySelector('#temp');
+const tempMin = document.querySelector('#min-temp');
+const tempMax = document.querySelector('#max-temp');
+const latitude = document.querySelector('#lat');
+const longitude = document.querySelector('#lon');
+const hum = document.querySelector('#hum');
+const pre = document.querySelector('#pre');
+const wind = document.querySelector('#wind');
+const windDegree = document.querySelector('#wind-temp');
+const uv = document.querySelector('#uv')
+const dayPeriodImg = document.querySelector('#day-period');
 let appid = '142241b72cb61b0e44cfbe4c5d335d6f';
 
-let ville_label = document.querySelector('#ville');
-let temp_label = document.querySelector('#temperature_label');
-let changer = document.querySelector('#changer');
 
-App();
-function App(){
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(position => {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        Start(lat, lon)
+        console.log(lon, lat)
+    }, (() => MeteoQuery('Yaounde')))
+}
+else {
+    MeteoQuery('Yaounde');
+}
+async function Start(lat, lon) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}&units=metric`;
+    let request = await fetch(url, { method: 'GET' })
+        .then((response) => {
+            if (response.ok) {
+                response.json().then((myJson) => {
+                    let datas = myJson;
+                    console.log(myJson);
+                    datasFill(datas);
+                })
+            }
+        })
+        .catch((e) => {
+            console.log(e.message);
+        })
+}
+async function MeteoQuery(ville) {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${appid}&units=metric`;
-    let request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.responseType = 'json';
-    request.send();
-    request.onload = ()=>{
-        console.log("c'est bon");
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                let response = request.response;
-                ville_label.textContent = ville;
-                temp_label.textContent = response.main.temp;
+    let request = await fetch(url, { method: 'GET' })
+        .then((response) => {
+            if (response.ok) {
+                response.json().then((myJson) => {
+                    let datas = myJson;
+                    console.log(datas);
+                    datasFill(datas);
+                })
             }
-            else{
-                alert('nous rencontrons un problème revenez plus tard');
-            }
-        }
+        })
+        .catch((e) => {
+            console.log(e.message);
+        })
 }
+
+const datasFill = (datas) => {
+    ville.textContent = datas.name;
+    pays.textContent = datas.sys.country;
+    latitude.textContent = datas.coord.lat;
+    longitude.textContent = datas.coord.lon;
+    temp.textContent = datas.main.temp;
+    tempMin.textContent = datas.main.temp_min;
+    tempMax.textContent = datas.main.temp_max;
+    hum.textContent = datas.main.humidity;
+    pre.textContent = datas.main.pressure;
+    wind.textContent = datas.wind.speed;
+    windDegree.textContent = datas.wind.deg;
+    let date = new Date();
+    console.log(date.getDate)
+    //uv.textContent = datas;
 }
-changer.addEventListener('click', ()=>{
-    ville = prompt('Veuillez saisir le nom de la ville dont vous voulez la température.');
-    App();
-})
-console.log(navigator);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleSubmit();
+});
+
+const handleSubmit = e => {
+    // e.preventDefault();
+    // e.stopPropagation();
+    villeRequest = document.querySelector('.ville-input').value;
+    document.querySelector('.ville-input').value = "";
+    MeteoQuery(villeRequest);
+    console.log(villeRequest);
+
+}
