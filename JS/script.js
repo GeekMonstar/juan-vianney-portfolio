e80026bb9b6b4299c2e83b214e346651008abe37
GeekMonstar <94204578+GeekMonstar@users.noreply.github.com>
2023-01-12 13:52:07 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -26,25 +26,25 @@ const competences = [
 ];
 const projectsData = [
     {
-        'link': '../Projects/AppliMeteo',
+        'link': '../Projects/AppliMeteo/index.html',
         'title': 'Apllication météo',
         'mobileImage': './img/cap-1-mobile.png',
         'pcImage': './img/cap-1-pc.png',
     },
     {
-        'link': '../Projects/SportApp',
+        'link': '../Projects/SportApp/index.html',
         'title': 'PlayerWiki',
         'mobileImage': './img/cap-2-mobile.png',
         'pcImage': './img/cap-2-pc.png',
     },
     {
-        'link': '../Projects/BitcoinPrice',
+        'link': '../Projects/BitcoinPrice/index.html',
         'title': 'Prix du Bitcoin',
         'mobileImage': './img/cap-3-mobile.png',
         'pcImage': './img/cap-3-pc.png',
     },
     {
-        'link': '../Projects/sun-moon',
+        'link': '../Projects/sun-moon/index.html',
         'title': 'éclipse',
         'mobileImage': './img/cap-4-mobile.png',
         'pcImage': './img/cap-4-pc.png',
