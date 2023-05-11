2061ec230f503951bccb3742e34e1dde22525519
Juan Vianney <94204578+GeekMonstar@users.noreply.github.com>
2023-05-11 18:47:35 +0200

    Add files via upload

---
+++ data/projectsData.js
@@ -0,0 +1,91 @@
+const projects_data = [
+    {
+        link: 'https://geekmonstar.github.io/AudioPlayer/',
+        title: 'Audio Player',
+        cover: './img/cap-6.png',
+        description: 'Lecteur audio personalisé qui comporte un visualiseur.',
+        skills: ['html', 'css', 'js'],
+        dev: false
+    },
+    {
+        link: ' https://geekmonstar.github.io/Lecteur-video-personnalise/',
+        title: 'Lecteur vidéo personnalisé',
+        cover: './img/cap-9.png',
+        description: 'Lecteur vidéo personalisé sans control de natif.',
+        skills: ['html', 'css', 'js'],
+        dev: false
+    },
+    {
+        link: 'https://geekmonstar.github.io/Linear-gradient-generator/',
+        title: 'Générateur de dégradé',
+        cover: './img/cap-7.png',
+        description: 'Générateur de dégradé dynamyque comportant une fontionnalité de copie du dégradé généré.',
+        skills: ['html', 'css', 'js'],
+        dev: false
+    },
+    {
+        link: 'https://geekmonstar.github.io/wikipedia-app/',
+        title: 'Recherche wikipedia',
+        cover: './img/cap-10.png',
+        description: "Utilisation de l'API de wikipedia pour proposer des resultat de recherche.",
+        skills: ['html', 'css', 'js'],
+        dev: false
+    },
+    {
+        link: 'https://geekmonstar.github.io/Quiz/',
+        title: 'Quiz',
+        cover: './img/cap-11.png',
+        description: "Quiz basique.",
+        skills: ['html', 'css', 'js'],
+        dev: false
+    },
+    {
+        link: 'https://geekmonstar.github.io/particles/',
+        title: 'Particules',
+        cover: './img/cap-8.png',
+        description: 'Particules animées créer grace au Canvas de HTML5.',
+        skills: ['html', 'css', 'js'],
+        dev: false
+    },
+    {
+        link: 'https://geekmonstar.github.io/weather/',
+        title: 'Application météo',
+        cover: './img/cap-1-pc.png',
+        description: "Site metéo prenant en compte la localisation lorsque vous l'accepté.",
+        skills: ['html', 'css', 'js'],
+        dev: false
+    },
+    {
+        link: 'https://geekmonstar.github.io/game-zone/',
+        title: 'Gaming zone(website)',
+        cover: './img/cap-5-pc.png',
+        description: "Site de presentation d'un lieu d'attraction vidéoludique.",
+        skills: ['html', 'css', 'js'],
+        dev: false
+    },
+    {
+        link: 'https://geekmonstar.github.io/wikifoot/',
+        title: 'PlayerWiki',
+        cover: './img/cap-2-pc.png',
+        description: '',
+        skills: ['html', 'css', 'js'],
+        dev: true
+    },
+    {
+        link: 'https://geekmonstar.github.io/bitcoin/',
+        title: 'Prix du Bitcoin',
+        cover: './img/cap-3-pc.png',
+        description: 'Obtener en temps réel la valeur en temps réel la valeur du bitcoin.',
+        skills: ['html', 'css', 'js'],
+        dev: false
+    },
+    {
+        link: 'https://geekmonstar.github.io/eclipse/',
+        title: 'éclipse',
+        cover: './img/cap-4-pc.png',
+        description: 'Animation en pur css.',
+        skills: ['html', 'css'],
+        dev: false
+    }
+];
+export default projects_data;
---
+++ src/data/competences.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var competences = [
+    {
+        title: 'HTML5',
+        icon: 'html',
+        level: 80,
+        color: 'orange'
+    },
+    {
+        title: 'CSS3',
+        icon: 'css',
+        level: 80,
+    },
+    {
+        title: 'JS',
+        icon: 'js',
+        level: 70
+    },
+    {
+        title: 'PHP',
+        icon: 'php',
+        level: 30
+    },
+    {
+        title: 'Base de donnée',
+        icon: 'db',
+        level: 50
+    }
+];
+exports.default = competences;
